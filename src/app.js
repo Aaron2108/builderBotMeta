5d34297c2a46b66c0121b7c81d7ce53bfe347bce
Oliver Mendez <mendezoliver2108@gmail.com>
2024-07-30 13:14:39 -0500

    as

--- src/app.js
+++ src/app.js
@@ -5,61 +5,110 @@ import { MetaProvider as Provider } from '@builderbot/provider-meta'
 
 const PORT = process.env.PORT ?? 3008
 
-const discordFlow = addKeyword('doc').addAnswer(
-    ['You can see the documentation here', '📄 https://builderbot.app/docs \n', 'Do you want to continue? *yes*'].join(
-        '\n'
-    ),
-    { capture: true },
-    async (ctx, { gotoFlow, flowDynamic }) => {
-        if (ctx.body.toLocaleLowerCase().includes('yes')) {
-            return gotoFlow(registerFlow)
-        }
-        await flowDynamic('Thanks!')
-        return
-    }
-)
 
-const welcomeFlow = addKeyword(['hi', 'hello', 'hola'])
-    .addAnswer(`🙌 Hello welcome to this *Chatbot*`)
-    .addAnswer(
-        [
-            'I share with you the following links of interest about the project',
-            '👉 *doca* to view the documentation',
-        ].join('\n'),
-        { delay: 800, capture: true },
-        async (ctx, { fallBack }) => {
-            if (!ctx.body.toLocaleLowerCase().includes('doc')) {
-                return fallBack('You should type *doc*')
-            }
-            return
-        },
-        [discordFlow]
+
+const welcomeFlow = addKeyword(EVENTS.WELCOME)
+    .addAction(
+        async (ctx, { flowDynamic, provider  }) => {
+            const to = ctx.from
+            await provider.sendFile(to, './assets/presentacion1TO.pdf')
+            console.log(sock);
+        }
     )
 
-const registerFlow = addKeyword(utils.setEvent('REGISTER_FLOW'))
-    .addAnswer(`What is your name?`, { capture: true }, async (ctx, { state }) => {
-        await state.update({ name: ctx.body })
-    })
-    .addAnswer('What is your age?', { capture: true }, async (ctx, { state }) => {
-        await state.update({ age: ctx.body })
-    })
-    .addAction(async (_, { flowDynamic, state }) => {
-        await flowDynamic(`${state.get('name')}, thanks for your information!: Your age: ${state.get('age')}`)
+const flowString = addKeyword('prueba')
+    .addAnswer('Estas son las categorías disponibles:', null, async (ctx, {flowDynamic}) => {
+        await flowDynamic('Enviar un mensaje text')
+        // Enviar una imagen o pdf o etc
+        await flowDynamic([
+            {
+                body:"soy una imagen",
+                media:'https://repositorio.uam.es/bitstream/handle/10486/698762/menus_montero_ALVIMED_2020.pdf?sequence=4',
+                delay:1000
+            }
+        ]) 
+        
     })
 
-const fullSamplesFlow = addKeyword(['samples', utils.setEvent('SAMPLES')])
-    .addAnswer(`💪 I'll send you a lot files...`)
-    .addAnswer(`Send image from Local`, { media: join(process.cwd(), 'assets', 'sample.png') })
-    .addAnswer(`Send video from URL`, {
-        media: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYTJ0ZGdjd2syeXAwMjQ4aWdkcW04OWlqcXI3Ynh1ODkwZ25zZWZ1dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LCohAb657pSdHv0Q5h/giphy.mp4',
+const cotizacion1 = addKeyword('1').addAnswer(`Send file from URL`, 
+     { media: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf' }
+ )
+
+const flow = addKeyword('flow1')
+    .addAction(async (_,{flowDynamic}) => {
+        // ...db get source...
+        await flowDynamic([
+            {body:'This is an pdf', media:'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'}
+        ])
+        await flowDynamic([
+            {body:'This is a video', media:'https://media.giphy.com/media/KWZKwdBC2ODWlQ8kgt/giphy.mp4'}
+        ])
     })
-    .addAnswer(`Send audio from URL`, { media: 'https://cdn.freesound.org/previews/728/728142_11861866-lq.mp3' })
-    .addAnswer(`Send file from URL`, {
-        media: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+
+    const flow2 = addKeyword('flow2')
+    .addAction(async (_,{flowDynamic}) => {
+        const pathLocal = join('assets','presentacion1TO.pdf')
+        // pathLocal = c:/doc.pdf
+        await flowDynamic([
+            {body:'This is a video', media: pathLocal }
+        ])
     })
+
+
+
+    const rangoInversion1 = addKeyword('1').addAnswer(['Indícame el rango de inversión que tienes proyectado:'
+        ,'*1*: S/10,000 a S/15,000'
+        ,'*2*: S/16 000 a S/24 000'
+        ,'*3*: S/25 000 a S/34 000'
+    ],null,null, [cotizacion1])
+
+
+    const rangoCotizacion1 = addKeyword('1').addAnswer(['Indícame el rango en m² que tiene tu ambiente:'
+    ,'*1*: 3m² a 5m²'
+    ,'*2*: 6m² a 10m²'
+    ,'*3*: 11m² a 15m²'
+    ,'*4*: 16m² a 20m²'
+    ], null,null, [rangoInversion1])
+
+    const flujoCotizacion1 = addKeyword('1')
+    .addAnswer(['Que ambiente necesitas'
+            ,'*1*: Sala comedor'
+            ,'*2*: Sala cocina'
+            ,'*3*: Sala dormitorio principal'
+            ,'*4*: Sala dormitorio secundario'
+            ,'*5*: Sala baño privado'
+            ,'*6*: Sala baño de visitas'
+        ], null,null,[rangoCotizacion1])
+
+        const flujoPrincipal = addKeyword(['Hola', 'hola', 'buenas'])
+        .addAnswer('hola que tal te saluda ... 😊')
+        .addAnswer('¿Cual es tu nombre?', {capture:true}, async (ctx,{flowDynamic}) =>{
+            // if(!ctx.body.includes('@')){
+            //     return fallBack()
+            // }
+            const nombre = ctx.body;
+            await flowDynamic([
+                {body:`Hola ${nombre}, en que te puedo ayudar hoy?`}
+                
+            ])
+        }).addAnswer(['*1*: Cotización de diseño por m²'
+            , '*2*: Cotización de implementación por depa completo m²'
+            ,'*3*: Cotización de implementación por ambiente m²'],null, null, [flujoCotizacion1])
+
+        
+        const flujoAdios = addKeyword(['gracias', 'adios', 'bye', 'chau']).addAnswer('hasta luego')
+
+
+        // {
+        //     buttons: [{ body: 'diseño' },
+        //             { body: 'implementacion'},
+        //             { body: 'implementacion'}],
+        // }
+
+
 
 const main = async () => {
-    const adapterFlow = createFlow([welcomeFlow, registerFlow, fullSamplesFlow])
+    const adapterFlow = createFlow([flujoPrincipal,flujoAdios, flow, flow2, flowString, welcomeFlow])
     const adapterProvider = createProvider(Provider, {
         jwtToken: 'EAAYgzcqAN3oBOxZBYiXvGgLkIqJlyrQCeootSWflbMIC7fNl0rP5vnB9WHIRHgoteto5OOZCv7CiAA3v1wYhdi4Xldx9ZCJgFPzPpTEsKeBn2s5zcNZC2KCMzSdxTHoAuHPmhVEirJM31NwlhhH1hpEKZCdNbExTFuPgHAnBlYZCK06i2A4PaZB2MCLLLYrpPkC1ZBdmdLJLzcKB46ctH12eiEJkzm2p63M22ecZD',
         numberId: '392059530655358',
